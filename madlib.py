9fa762aa352ef3d41f6d35bbe58f0d0717b34642
rreiko <sydnee.ramirez1@marist.edu>
2018-09-13 14:52:04 -0400

    Solutiont to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,7 @@
+
+noun = input ("Enter a noun: ")
+verb = input ("Enter a verb: ")
+adjective = input ("Enter an adjective: ")
+place = input ("Enter a place: ")
+
+print("I can't wait to hold my " + noun + " and " + verb + " to our favorite " + adjective + " " + place + ".")
