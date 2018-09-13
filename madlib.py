c9de77edacc263859a7b707a46e98a9bb21c6a6e
rreiko <sydnee.ramirez1@marist.edu>
2018-09-13 15:21:11 -0400

    Solution to Mad Libs exercise.

--- madlib.py
+++ madlib.py
@@ -1,7 +1,18 @@
+def promptForWords():
+    global noun, verb, adjective, place
+    noun = input ("Enter a noun: ")
+    verb = input ("Enter a verb: ")
+    adjective = input ("Enter an adjective: ")
+    place = input ("Enter a place: ")
 
-noun = input ("Enter a noun: ")
-verb = input ("Enter a verb: ")
-adjective = input ("Enter an adjective: ")
-place = input ("Enter a place: ")
 
-print("I can't wait to hold my " + noun + " and " + verb + " to our favorite " + adjective + " " + place + ".")
+def makeAndPrintSentence():
+    print("I can't wait to hold my " + noun + " and " + verb + " to our favorite " + adjective + " " + place + ".")
+
+
+def main():
+    promptForWords()
+    makeAndPrintSentence()
+
+
+main()
