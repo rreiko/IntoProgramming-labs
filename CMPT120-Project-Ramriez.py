4ea733a389ec131887b529a3bc30138e015f01b5
rreiko <sydnee.ramirez1@marist.edu>
2018-09-19 13:27:16 -0400

    remeber to change file name

---
+++ CMPT120-Project-Ramriez.py
@@ -0,0 +1,29 @@
+# Introduction to Programming
+# Author: Sydnee Ramirez
+# Date: September 17 2018
+
+name = input ("Aloha playa, please enter your name.")
+print ("Nice to meet you " + name + ".")
+
+answer = input("Are you ready to take a trip to Hawai'i, yah or nah?")
+
+if answer == "yah":
+    print("+5 points")
+
+elif answer == "nah":
+    print("Shootz boi")
+
+else:
+    print("Please answer yah or nah.")
+
+print("Welocme to the isalnd of Oahu. People have always told you about the lushest plants and pristine water. "
+    "What people don’t tell you about are the dangers lurking around every corner. Killer coconuts, hungry sharks, and volcanic activity. "
+    "All of these factors can make a vacation difficult to enjoy. In order to go cruise at China Walls with the locals"
+    " you must make it through the unpredictable journey to the best spot on the island."
+    " Once at the beach you will have the opportunity to watch one of the most breath taking sunsets in the world.")
+
+
+input("Press enter to continue")
+
+
+print("yerrrrr")
