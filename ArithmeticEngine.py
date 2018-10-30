935907300cb8dfe4be60c6fd160354ee807a0b76
rreiko <sydnee.ramirez1@marist.edu>
2018-10-30 16:33:12 -0400

    final commit

--- ArithmeticEngine.py
+++ ArithmeticEngine.py
@@ -15,8 +15,12 @@ def doLoop():
     while True:
         cmd = input("What computation do you want to perform? ").lower()
         if cmd=="add" or cmd=="sub" or cmd == "mult" or cmd == "div" or cmd == "power":
-            num1 = int(input("Enter the first number: "))
-            num2 = int(input("Enter the second number: "))
+            try:
+                num1 = int(input("Enter the first number: "))
+                num2 = int(input("Enter the second number: "))
+            except:
+                print("Not a number")
+                continue
             if cmd == "add":
                 result = num1 + num2
             elif cmd == "sub":
